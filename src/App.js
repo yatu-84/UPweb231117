df8dd122790655fc4a144959c1f248e29b2b5891
yatu-84 <kariyaseita@gmail.com>
2023-11-17 20:18:57 +0900

    modified:   docs/asset-manifest.json 	modified:   docs/index.html 	renamed:    docs/static/js/main.ebe84fc6.js -> docs/static/js/main.f40bc9a1.js 	renamed:    docs/static/js/main.ebe84fc6.js.LICENSE.txt -> docs/static/js/main.f40bc9a1.js.LICENSE.txt 	renamed:    docs/static/js/main.ebe84fc6.js.map -> docs/static/js/main.f40bc9a1.js.map 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        way
+        wayy
         <a
           className="App-link"
           href="https://reactjs.org"
