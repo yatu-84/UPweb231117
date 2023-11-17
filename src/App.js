4b8db3dec5a7805ad6ba07bb649f96b6bd1141dd
yatu-84 <kariyaseita@gmail.com>
2023-11-18 02:56:33 +0900

    modified:   docs/asset-manifest.json 	modified:   docs/index.html 	deleted:    docs/static/css/main.341aa50d.css 	deleted:    docs/static/cssmain.341aa50d.css.map 	new file:   docs/static/cssmain.c982c440.css 	new file:   docs/static/css/main.c982c440.css.map 	renamed:    docs/static/js/main.40d86ae7.js -> docs/static/js/main.2587737c.js 	renamed:    docs/static/js/main.40d86ae7.js.LICENSE.txt -> docs/static/js/main.2587737c.js.LICENSE.txt 	new file:   docs/static/js/main.2587737c.js.map 	deleted:    docs/static/js/main.40d86ae7.js.map 	modified:   src/App.js 	modified:   src/css/header.css 	modified:   src/css/home.css

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './js/header'; 
 import Home from './js/home'; 
+import background from './img/back.jpg'
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <header className="App-header">
-          <Header />
-          <Router>
-            <Routes>
-              <Route path="/UPweb231117/" element={<Home />} />
-            </Routes>
-          </Router>
-        </header>
+      <div className='App' style={{ backgroundImage: `url(${background})` }}>
+          <header className="App-header">
+            <Header />
+            <Router>
+              <Routes>
+                <Route path="/UPweb231117/" element={<Home />} />
+              </Routes>
+            </Router>
+          </header>
       </div>
     );
   }
