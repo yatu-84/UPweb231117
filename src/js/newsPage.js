579faf507895dbafad86805e474d9414593d949d
yatu-84 <kariyaseita@gmail.com>
2023-11-20 22:03:05 +0900

    On branch master  Changes to be committed: 	modified:   docs/asset-manifest.json 	modified:   docs/index.html 	new file:   docs/static/css/main.d52f58bd.css 	new file:   docs/static/css/main.d52f58bd.css.map 	deleted:    docs/static/css/main.fed72fe7.css 	deleted:    docs/static/css/main.fed72fe7.css.map 	deleted:    docs/static/js/main.76fa1e2e.js.map 	renamed:    docs/static/js/main.76fa1e2e.js -> docs/static/js/main.e13f2dc5.js 	renamed:    docs/static/js/main.76fa1e2e.js.LICENSE.txt -> docs/static/js/main.e13f2dc5.js.LICENSE.txt 	new file:   docs/static/js/main.e13f2dc5.js.map 	modified:   src/App.css 	modified:   src/css/footer.css 	modified:   src/css/header.css 	modified:   src/css/newsPage.css 	modified:   src/css/schedulePage.css 	modified:   src/js/newsPage.js

--- src/js/newsPage.js
+++ src/js/newsPage.js
@@ -5,7 +5,48 @@ class newsPage extends React.Component {
   render() {                          
       return (
             <div className='newsPage'>
-                content news
+                <div className='sampleSheet'>
+                    <section>
+                        <p>ホームページ開設！</p>
+                        <div className='text'>
+                            <br/>
+                            ホームページを開設しました。
+                        </div>
+                    </section>
+                </div>
+
+                <div className='sampleSheet'>
+                    <section>
+                        <p>news2</p>
+                        <div className='text'>
+                            <br/>
+                            mazilove
+                            <br/>
+                            kick
+                            <br/>
+                            hoge
+                            <br/>
+                            hogehoge
+                            <br/>
+                            super
+                            <br/>
+                            final
+                            <br/>
+                            123456789012345678901234567890
+                            <br/>
+                            qwertyuiopppp
+                            <br/>
+                            qwertyuiopppp
+                            <br/>
+                            qwertyuiopppp
+                            <br/>
+                            qwertyuiopppp
+                            <br/>
+                            qwertyuiopppp
+                        </div>
+                    </section>
+                </div>
+
             </div>
         );
     }
